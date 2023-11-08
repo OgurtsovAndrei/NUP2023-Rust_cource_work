da6286f262ad485532458515fd11e19ac07d81d3
Ogurtsov_Andrey <avogurtsov@edu.hse.ru>
2023-11-08 21:10:02 +0200

    Load 2-nd HW

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
                 eprintln!("It is not a directory: {}", path);
                 return;
             }
-            let (res, body) = process_dir(&path, "", &*find_substr);
+            let (res, body) = process_dir(&path, "", &find_substr);
             if res { println!("{}", body) }
         }
         Err(_) => {
@@ -20,17 +20,17 @@ fn main() {
     }
 }
 
-fn process_dir(path: &str, prefix_print: &str, find_substr: &str) -> (bool, String) {
+fn process_dir(path: &str, prefix_print: &str, find_substr: &String) -> (bool, String) {
     let mut is_not_empty = false;
-    let mut body: String = String::from("");
+    let mut body: String = empty_string();
     if let Ok(entries) = fs::read_dir(path) {
         for entry in entries {
             let (res, res_body) = process_entry(entry, prefix_print, find_substr);
             is_not_empty |= res;
-            if res { body.push_str(&*res_body) }
+            if res { body.push_str(&res_body) }
         }
     } else {
-        body.push_str(&*(String::from(prefix_print) + "Problem occurred during reading the directory\n"))
+        body.push_str(&(format!("{prefix_print}Problem occurred during reading the directory\n")))
     }
     return (is_not_empty, body);
 }
@@ -44,7 +44,7 @@ fn get_directory_from_cli_args() -> (String, String) {
     }
     let path = args[1].clone();
 
-    let mut find_substr = String::from("");
+    let mut find_substr = empty_string();
     if args.len() >= 4 {
         if args[2] == "--find" { find_substr = args[3].to_string() }
     }
@@ -54,28 +54,32 @@ fn get_directory_from_cli_args() -> (String, String) {
 
 fn process_entry(entry: Result<DirEntry, std::io::Error>, prefix_print: &str, find_substr: &str) -> (bool, String) {
     let mut is_not_empty = false;
-    let mut body: String = String::from("");
+    let mut body= empty_string();
     let entry = entry.expect("Failed to read directory entry\n");
     let entry_path = entry.path();
     if entry_path.is_file() {
         if let Some(file_name) = entry.file_name().to_str() {
             if file_name.contains(find_substr) {
-                body.push_str(&*format!("File\t: {prefix_print}{file_name}\n"));
+                body.push_str(&format!("File\t: {prefix_print}{file_name}\n"));
                 is_not_empty = true
             }
         }
     } else if entry_path.is_dir() {
         if let Some(dir_name) = entry.file_name().to_str() {
-            let new_prefix: String = prefix_print.to_string() + "  | ";
+            let new_prefix: String = format!("{prefix_print}  | ");
             let mut new_subst: String = find_substr.to_string();
-            if dir_name.contains(find_substr) { new_subst = String::from("") }
-            let (res, res_body) = process_dir(entry_path.to_str().unwrap(), &*new_prefix, &*new_subst);
+            if dir_name.contains(find_substr) { new_subst = empty_string() }
+            let (res, res_body) = process_dir(entry_path.to_str().unwrap(), &new_prefix, &new_subst);
             is_not_empty |= res;
             if res {
-                body.push_str(&*format!("Dir\t: {prefix_print}{dir_name}\n"));
-                body.push_str(&*res_body)
+                body.push_str(&format!("Dir\t: {prefix_print}{dir_name}\n"));
+                body.push_str(&res_body)
             }
         }
     }
     return (is_not_empty, body);
+}
+
+fn empty_string() -> String {
+    String::from("")
 }
